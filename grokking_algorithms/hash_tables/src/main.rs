9e2e1f125669d869dbb1d1826693e7f73d49ee89
Luis <luis.vi.fer@gmail.com>
2023-12-21 15:02:57 +0100

    added custom implementation of hashtable in rust

--- grokking_algorithms/hash_tables/src/main.rs
+++ grokking_algorithms/hash_tables/src/main.rs
@@ -1,3 +1,81 @@
-fn main() {
-    println!("Hello, world!");
+
+use std::io;
+mod my_hash_table;
+
+use my_hash_table::Hashable;
+
+pub use self::my_hash_table::MyHashTable;
+
+
+
+pub struct Pool {
+    voted: MyHashTable<String,bool>,
 }
+
+trait Voted {
+   fn check_voter(&mut self,name:String)-> Result<String,String>;
+}
+
+impl Hashable for String {
+    fn hash(&self) -> usize {
+        self.len() * self.as_bytes().len() * 122000
+    }
+}
+
+impl Voted for Pool {
+     fn check_voter(&mut self, name:String) -> Result<String,String>{
+    
+        match self.voted.find(&name) {
+            Some(&true) => Err(format!("Kick them out {}", name)),
+            Some(&false) => {
+                self.voted.put(name.clone(), true);
+                Ok(format!("let them vote! {}", name))
+            },
+            None => {
+                self.voted.insert(name.clone(), true);
+                Ok(format!("let them vote! {}", name))
+            }
+        }
+       
+    }
+    
+}
+
+impl Pool {
+    pub fn new() -> Pool{
+        Pool {
+            voted : MyHashTable::default()
+        }
+    }
+}
+
+
+
+fn main() {
+     let mut pool = Pool::new();
+      (0.. 10).for_each(|_| {
+        match read_name("Give me a new voter:") {
+            Ok(voter) => match pool.check_voter(voter){
+                Ok(value) =>  println!("{}", value),
+                Err(value) =>  println!("{}", value),
+            },
+            Err(e) => println!("Error: {}", e),
+        }
+      });
+
+}
+
+// Función para leer y validar un valor booleano
+fn read_name(msg: &str) ->  Result<String,String> {
+    let mut input = String::new();
+
+    println!("{}", msg);
+
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => Ok(input.trim().to_lowercase()), // Si la lectura es exitosa
+        Err(_) => Err("Error al leer la línea".to_string()), // En caso de error
+    }
+}
+
+
+
